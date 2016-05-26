f5b0469cfde8d28d9a65a3a4bf3c69c0e3dabeed
denver daniels <denver@projectcodex.co>
2016-05-26 15:34:56 +0200

    first route

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
-const express = require('express');
-app = express();
-app.listen(3000);
-app.get('/', function(req,res){
-res.send('hello');
+const express    = require('express'),
+      app        = express(),
+      mongojs    = require('mongojs');
+      db         = mongojs('contactlist', ['contactlist'])
+      bodyParser = require('body-parser');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+app.get('/contactlist', function (req, res) {
+  console.log('I received a GET request');
+
+  db.contactlist.find(function (err, docs) {
+    console.log(docs);
+    res.json(docs);
+  });
 });
-console.log('app running on port 3000');
+
+app.listen(3000);
+console.log("Server running on port 3000");
